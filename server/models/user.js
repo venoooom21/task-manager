9bdb462579e0b01930250b102ce77dd2c4391534
Wafa Allouche <allouchewafa@gmail.com>
2024-10-01 01:03:43 +0100

    user routes and controller

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    //tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
   },
   { timestamps: true }
 );
